4f82533e13eacc78b47a972395b7e27422f087a6
Bedevire <bedevire@gmail.com>
2022-04-30 07:17:59 +0200

    Add new pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WatchlistRegistrationComponent } from './watchlist-registration/watchlist-registration.component';
+import { WatchlistSearchComponent } from './watchlist-search/watchlist-search.component';
+import { MembersComponent } from './members/members.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'resitration-route', component: WatchlistRegistrationComponent},
+  {path: 'search-route', component: WatchlistSearchComponent},
+  {path: 'debug-route', component: MembersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
